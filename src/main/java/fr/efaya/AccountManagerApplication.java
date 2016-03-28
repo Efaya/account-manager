64e1a7c9950132c9214e6cce16a222c082761186
Efaya <sahbi.ktifa@gmail.com>
2016-03-28 21:39:02 +0200

    First commit

---
+++ src/main/java/fr/efaya/AccountManagerApplication.java
@@ -0,0 +1,12 @@
+package fr.efaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagerApplication.class, args);
+	}
+}
